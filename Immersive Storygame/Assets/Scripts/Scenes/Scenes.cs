1d0b648f3ae7b6143fcdd2f4f0deb8d0e9314faa
jaschahuisman <jascha.huisman@gmail.com>
2019-11-26 10:07:02 +0100

    UI Manager 1.2
    • UI Manager 1.2
    • Text Mesh Pro + Fonts

---
+++ Immersive Storygame/Assets/Scripts/Scenes/Scenes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour
+{
+    // Define UI Manager GameObject
+    // GameObject UI_Object;
+    // UserInterface UI;
+
+    // Start the Game
+    void Start()
+    {
+        // Get UserInterface Class of UI_Object
+        // UI_Object = GameObject.FindWithTag("UI_Manager");
+        // UI = UI_Object.GetComponent<UserInterface>();
+    }
+
+    public void ChangeToScene(string sceneToChangeTo)
+    {
+        SceneManager.LoadScene(sceneToChangeTo);
+    }
+}
---
+++ Immersive Storygame/Assets/Scripts/UI/StartScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreen : MonoBehaviour
+{
+    // Define Scene Manager GameObject
+    GameObject SceneObject;
+    Scenes scene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get UserInterface Class of UI_Object
+        SceneObject = GameObject.FindWithTag("GameManager");
+        scene = SceneObject.GetComponent<Scenes>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool userInput = Input.GetKeyDown("space");
+        if (userInput == true)
+        {
+            scene.ChangeToScene("Scene02_Prologue");
+        }
+
+    }
+}
