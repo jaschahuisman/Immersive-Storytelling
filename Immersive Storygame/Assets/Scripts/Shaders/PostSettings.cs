e7d5304e93bf90a7abc7c0c74d6d8d3df1bc62be
jaschahuisman <jascha.huisman@gmail.com>
2019-11-26 20:19:50 +0100

    Shading Post Processing 0.6

---
+++ Immersive Storygame/Assets/Scripts/Shaders/PostSettings.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PostSettings : MonoBehaviour
+{
+    public PostProcessVolume activeVolume;
+
+    public void toggleCG(bool value)
+    {
+        ColorGrading colorGrading;
+        activeVolume.profile.TryGetSettings(out colorGrading);
+
+        if (value)
+        {
+            colorGrading.active = true;
+        }
+        else 
+        {
+            colorGrading.active = false;
+        }
+    }
+
+    // public PostProcessProfile effects;
+    // public float vignette = 0f;
+    // public float saturation = 0f;
+
+}
