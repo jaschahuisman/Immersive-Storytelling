57bbf9921bff8484390bbdd3cf3f8aa194fc74fe
jaschahuisman <jascha.huisman@gmail.com>
2019-11-27 11:47:38 +0100

    Shader Post Processing 0.8
    Added Vignette

--- Immersive Storygame/Assets/Scripts/Shaders/PostSettings.cs
+++ Immersive Storygame/Assets/Scripts/Shaders/PostSettings.cs
@@ -5,24 +5,45 @@ using UnityEngine.Rendering.PostProcessing;
 
 public class PostSettings : MonoBehaviour
 {
+    // ShaderManager
+    public PostProcessVolume activeVolume;
+
+    // Color Grading (Saturation)
+    ColorGrading colorGrading;
     public float currentSaturation;
+    [Range (-100.0f, 100.0f)]
     public float targetSaturation;
     public float saturationSpeed;
-    public PostProcessVolume activeVolume;
-    ColorGrading colorGrading;
+
+    // Vignette
+    Vignette vignette;
+    public float currentVignetteIntensity;
+    [Range (0.0f, 1.0f)]
+    public float targetVignetteIntensity;
+    public float vignetteIntensitySpeed;
 
     public void Start()
     {
         activeVolume.profile.TryGetSettings(out colorGrading);
+        activeVolume.profile.TryGetSettings(out vignette);
     }
 
     public void Update()
     {
+        // Update current float values
         if (currentSaturation != colorGrading.saturation.value) { currentSaturation = colorGrading.saturation.value; }
+        if (currentVignetteIntensity != vignette.intensity.value) { currentVignetteIntensity = vignette.intensity.value; }
+
+        // Set new PostProcess colorGrading saturation values
         if (currentSaturation != targetSaturation)
         {
-            // colorGrading.saturation.value = targetSaturation;
             colorGrading.saturation.value = Mathf.Lerp(currentSaturation, targetSaturation, saturationSpeed * Time.deltaTime);
+        }
+
+        // Set new PostProcess vignette intensity values
+        if (currentVignetteIntensity != targetVignetteIntensity)
+        {
+            vignette.intensity.value = Mathf.Lerp(currentVignetteIntensity, targetVignetteIntensity, vignetteIntensitySpeed * Time.deltaTime);
         }
     }
 
@@ -31,9 +52,13 @@ public class PostSettings : MonoBehaviour
         targetSaturation = target;
     }
 
+    public void setTargetVignetteIntensity(float target)
+    {
+        targetVignetteIntensity = target;
+    }
+
     public void toggleCG()
     {
-        print(colorGrading.active);
         if (colorGrading.active == true)
         {
             colorGrading.active = false;
@@ -41,6 +66,18 @@ public class PostSettings : MonoBehaviour
         else if (colorGrading.active == false)
         {
             colorGrading.active = true;
+        }
+    }
+
+    public void toggleVignette()
+    {
+        if (vignette.active == true)
+        {
+            vignette.active = false;
+        }
+        else if (vignette.active == false)
+        {
+            vignette.active = true;
         }
     }
 }
