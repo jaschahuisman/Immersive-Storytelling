8b46c3f0f033026f5bcab3a288344eabe3819b30
jaschahuisman <jascha.huisman@gmail.com>
2019-11-27 13:03:21 +0100

    Merged Sounds.cs into Audiomanager.cs

--- Immersive Storygame/Assets/Scripts/Sound/AudioManager.cs
+++ Immersive Storygame/Assets/Scripts/Sound/AudioManager.cs
@@ -3,6 +3,20 @@
 using System;
 using UnityEngine;
 
+[System.Serializable]
+public class Sound {
+    public AudioClip clip;
+    public string name;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(.1f, 3f)]
+    public float pitch;
+ 
+    [HideInInspector]
+    public AudioSource source;
+}
+
 //volg de video :)
 //Maak eerst een nieuw object aan en maak dat een audio manager
 public class AudioManager : MonoBehaviour 
