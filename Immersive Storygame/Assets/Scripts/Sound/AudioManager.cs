9e0ede3b2f301d8398940c19dad7f9df0a7e0a0d
jaschahuisman <jascha.huisman@gmail.com>
2019-12-03 17:52:36 +0100

    AudioManager 1.1

--- Immersive Storygame/Assets/Scripts/Sound/AudioManager.cs
+++ Immersive Storygame/Assets/Scripts/Sound/AudioManager.cs
@@ -1,57 +1,58 @@
-﻿using UnityEngine.Audio;
-//Dit hieronder is voor het onderste gedeelte, de public void
-using System;
+﻿using System;
 using UnityEngine;
+using UnityEngine.Audio;
 
 [System.Serializable]
 public class Sound
 {
+    // Sound AudioClip file
     public AudioClip clip;
+
+    // Sound name
     public string name;
 
+    // Sound volume
     [Range(0f, 1f)]
     public float volume;
+
+    // Sound pitch
     [Range(.1f, 3f)]
     public float pitch;
+
+    // Sound loop
+    public bool loop;
 
     [HideInInspector]
     public AudioSource source;
 }
 
-//volg de video :)
-//Maak eerst een nieuw object aan en maak dat een audio manager
 public class AudioManager : MonoBehaviour
 {
 
     public Sound[] sounds;
     public static AudioManager instance;
 
-    //use this for initialization
-    //Ik heb dit veranderd naar Awake zoals in de video
     void Awake()
     {
-        if (instance == null) { instance = this; }
-        else { Destroy(gameObject); }
-
-        DontDestroyOnLoad(gameObject);
-
         foreach (Sound s in sounds)
         {
-            //s.source is later toegevoegd om te zoeken naar die s sound hierboven
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
-
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
         }
     }
 
-    //update is called once per frame
-    //KIJK HIER, ik heb update in comments gezet aangezien hij een nieuwe manier gebruikt om dingen aan te sturen
-    //void update (){
     public void Play(string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
         s.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Stop();
     }
 }
