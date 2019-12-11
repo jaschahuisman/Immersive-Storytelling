6512068f35b45379fad83a8c30f46e2ffaf93348
jaschahuisman <jascha.huisman@gmail.com>
2019-12-11 08:39:39 +0100

    Lighting Setup 0.1

---
+++ Immersive Storygame/Assets/Scripts/Scenes/MainSceneManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainSceneManager : MonoBehaviour
+{
+    GameObject UserInterfaceObj;
+    UserInterface userInterFaceManager;
+    GameObject AudioManagerObj;
+    AudioManager audioManager;
+
+    void Start()
+    {
+        UserInterfaceObj = GameObject.FindGameObjectWithTag("UI_Manager");
+        userInterFaceManager = UserInterfaceObj.GetComponent<UserInterface>();
+        AudioManagerObj = GameObject.FindGameObjectWithTag("AudioManager");
+
+        if (AudioManagerObj != null)
+        {
+            audioManager = AudioManagerObj.GetComponent<AudioManager>();
+            audioManager.Stop("Prologue_Mud_Music");
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
